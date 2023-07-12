f89982244bcf3087eae635cce444ebfab7256ebb
pyojeyn <jeynpyo@gmail.com>
2023-07-12 20:24:18 +0900

    리덕스 고급 단계 (http요청)

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+
+  const showCart = useSelector((state) => state.ui.cartIsVisable);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart)); // 내가 직접 따로 만든 Javascript 함수도 사용 가능
+    }
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
